98912e2071ac2bceb21033a9561a7405d0556ad8
John Olmsted <john@olmsted.io>
2019-03-25 19:17:18 -0700

    Response body is now JSON
    We can shove useful info in a structured way into the response body now.

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http')
-const os = require("os")
 
 const port = 8080
 
+const responseBody = (req) => {
+  return {
+    message: "Hello there",
+    requestIp: req.connection.remoteAddress
+  }
+}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(responseBody(req)))
 })
 
 server.listen(port, () => {
-  console.log(`Server running at http://${os.hostname()}:${port}`)
+  console.log(`Server running on port ${port}...`)
 })
